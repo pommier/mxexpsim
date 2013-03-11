72e99613574ee0eed23f8c894626fb9a15cd88d1
Jonathan Pommier <pommier@rnice39.esrf.fr>
2013-03-11 10:56:58 +0100

    Mx simulator with tango

---
+++ MxSim/src/org/esrf/mxsim/ContextClass.java
@@ -0,0 +1,5 @@
+package org.esrf.mxsim;
+
+public class ContextClass {
+
+}
---
+++ MxSim/src/org/esrf/mxsim/GenerationClass.java
@@ -0,0 +1,13 @@
+package org.esrf.mxsim;
+
+public class GenerationClass {
+	public GenerationClass() {
+		// TODO Auto-generated constructor stub
+	};
+	
+	public void copyImages(){
+		
+	}
+	
+	
+}
